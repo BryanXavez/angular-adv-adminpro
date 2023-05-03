1289f1f7a7769535858583ff07d474f224d3c05d
Bryan Orellana <odairchavez13@gmail.com>
2023-05-03 11:50:10 -0600

    seccion 7

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent { 
 
+ // constructor (private router: Router){}
+
+  login(){
+   // this.router.navigateByUrl('/');
+  }
 }
--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -10,6 +10,7 @@ import { Component } from '@angular/core';
 })
 export class Grafica1Component {
 public labels1: string[] = ['Donload Sales', 'In-Store Sales', 'Mail-Order Sales'];
+
 public data1 =  [
     { data: [ 350, 450, 100 ] },
     { data: [ 50, 150, 120 ] },
